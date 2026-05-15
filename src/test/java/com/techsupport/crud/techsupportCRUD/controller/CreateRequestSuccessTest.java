060c3aa9ae724b46e80b311a9f06cd7ff214a92f
lmodory <lmodory@wgu.edu>
2026-05-15 16:45:45 -0500

    Fixed Errors

--- src/test/java/com/techsupport/crud/techsupportCRUD/controller/CreateRequestSuccessTest.java
+++ src/test/java/com/techsupport/crud/techsupportCRUD/controller/CreateRequestSuccessTest.java
@@ -5,7 +5,6 @@ import com.techsupport.crud.techsupportCRUD.service.TechnicianService;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
--- src/test/java/com/techsupport/crud/techsupportCRUD/controller/LoginControllerTest.java
+++ src/test/java/com/techsupport/crud/techsupportCRUD/controller/LoginControllerTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
